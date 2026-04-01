f585fa376b5f28984c7925eebcf745fe1e580d89
Flo-whatever <117266986+Flo-whatever@users.noreply.github.com>
2026-04-02 00:30:02 +0200

    Add check for refunded amount in payment status

--- api/webhook-mollie.js
+++ api/webhook-mollie.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   try {
     const payment = await mollie.payments.get(id);
 
-    if (payment.status === 'paid') {
+    if (payment.status === 'paid' && parseFloat(payment.amountRefunded?.value || 0) === 0) {
       const meta = payment.metadata || {};
       const customerName    = meta.customerName    || 'Client';
       const customerEmail   = meta.customerEmail   || '';
